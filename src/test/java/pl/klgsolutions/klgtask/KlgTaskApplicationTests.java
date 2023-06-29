a2528c4823d209fff9bf51a2c608acd9ce454e29
Jakub <kuba.zm18@gmail.com>
2023-06-29 18:48:27 +0200

    Setup project

---
+++ src/test/java/pl/klgsolutions/klgtask/KlgTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klgsolutions.klgtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlgTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
